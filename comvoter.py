d0c270b709fd0f0cdcd001804ba3712de1c8858e
idikuci <steemnsfw@gmail.com>
2018-04-04 16:28:42 +1000

    Improved previous voting check. Increased time delay between votes

--- comvoter.py
+++ comvoter.py
@@ -1,20 +1,28 @@
 #from steem import Steem
 from piston.steem import Steem
 from piston.account import Account
+from piston.post import Post
 import datetime
 import time
+import json
 
 from keys import Posting_Key
 
-#import sleep
+accountname = 'idikuci'
+#accountname = 'comedyopenmic'
 
 nodes = ["wss://rpc.steemviz.com", "wss://rpc.steemliberator.com", "wss://steemd.minnowsupportproject.org"]
 pattern = '%Y-%m-%dT%H:%M:%S'
 
 
-MaxVP = 97
-# cutt off time in seconds [ 6 days X 24 hrs X 60 mins X 60 secs = 518400seconds]
-cutofftime = 518400
+MaxVP = 90
+# cut off time in seconds [ 6 days X 24 hrs X 60 mins X 60 secs = 518400 seconds]
+#cutofftime = 518400
+# cut off time in seconds [ 5.5 days X 24 hrs X 60 mins X 60 secs = 475200 seconds]
+cutofftime = 475200
+# cut off time in seconds [ 5.5 days X 24 hrs X 60 mins X 60 secs = 432000 seconds]
+cutofftime = 432000
+
 
 
 def getactiveVP(account):
@@ -57,47 +65,33 @@ def getupvotecandidate(account,s):
         account: A Steem Account object.
         s      : SteemInstance
     Returns:
-        permlink of oldest post within voting window not already voted on
+        identifier of posts/comments within voting window not already voted on
     """
     # Make sure we have the latest data
     account.refresh()
     tmpepochlastvote = 0
-    # Get last 100 votes from account
+    # Get last 2000 votes from account
     history = account.history2(filter_by='comment', take=2000)
-#    print("candidate")
-#    print(history)
 
-
-    voteshistory = account.history2(filter_by='vote', take=2000)
-    # Get list of things We've already voted on
-    votedlist = []
-    for event in voteshistory:
-       # Not really needed due to filter
-        if(event['type'] == "vote"):
-            # Make sure we are the one voting
-            if(event['voter'] == account.name):
-                votedlist.append(event['permlink'])
-
-    tmpoldest = 0
     currenttime = UtcNow()
     oldest_id = []
-
+    ignorepost = False
     for event in history:
        # Not really needed due to filter
-       # print("event")
         if(event['type'] == 'comment'):
             # Make sure we are the one voting
             if(event['author'] == account.name):
-                if (event['parent_author']): # If it's a comment
-                    if (event['permlink'] not in votedlist):
-                        # Time of post
-                        epochlastvote = (time.mktime(time.strptime(event['timestamp'], pattern)))
-                        elapsedtime = currenttime - epochlastvote
-                        if elapsedtime < cutofftime: # Is post in within time limit (6.5days default)
-                   #         print(event)
-#                            if elapsedtime > tmpoldest: # if it's the oldest we've found
-#                                tmpoldest = epochlastvote
-                                oldest_id.append("@" + event['author'] + "/" + event['permlink']) # get link to posts
+                epochlastvote = (time.mktime(time.strptime(event['timestamp'], pattern)))
+                elapsedtime = currenttime - epochlastvote
+                if elapsedtime < cutofftime: # Is post in within time limit (6.5days default)
+                    identifier = "@" + event['author'] + "/" + event['permlink']
+                    postid = Post(identifier,s) # Get comment info
+                    for voterid in postid['active_votes']: # check if we have already voted
+                        if (voterid['voter'] ==  account.name): # if already voted don't vote again
+                            ignorepost = True
+                            break
+                    if not ignorepost:
+                        oldest_id.append(identifier) # store link to posts
 
 
 
@@ -114,17 +108,17 @@ s = Steem(node=nodes)
 upvoter = Steem(node=nodes, wif=Posting_Key)
 
 # Initialize VP
-account = Account("comedyopenmic", s)
+account = Account(accountname, s)
 VP = getactiveVP(account)
 
 while True: # Loop continuously
     while VP < MaxVP: # If VP is below MaxVP go to sleep
         sleeptime = ( MaxVP - VP + 0.01 ) * (86400 / 20) # Time to sleep til we're above the MaxVP if no further votes are made
-        print(" VP = " + str(VP) + "; Sleeptime = " + str(sleeptime))
+        print(" VP = " + str(VP) + "; Sleeptime = " + str(sleeptime) + ' Going to Sleep Now! NapTime!')
 
         time.sleep(sleeptime) # Sleep
         # Grab VP again
-        account = Account("comedyopenmic", s)
+        account = Account(accountname, s)
         VP = getactiveVP(account)
 
     # Get oldest comment /post authored
@@ -132,8 +126,9 @@ while True: # Loop continuously
     for post_ID in posts:
         try:
             print("voting on old post: " , post_ID)
-            upvoter.vote(post_ID, 100, voter="comedyopenmic")
-            time.sleep(4) # sleep for 4 seconds make sure we don't vote too quickly
+            upvoter.vote(post_ID, 100, voter=accountname)
+            print('Successfully Voted... See you in 10 Mins')
+            time.sleep(600) # sleep for 10 minutes make sure we don't vote too  quickly and piston has time to update
         except:
             print("Already voted on this one try again")
 
@@ -143,5 +138,5 @@ while True: # Loop continuously
             break
 
 
-#    time.sleep(600)
+
 
